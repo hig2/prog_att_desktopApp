420855a97ab36af5fa94778d8eb8c76bfd755bee
Alexander ShvetsÐ <box0259@gmail.com>
2022-10-14 12:56:16 +0400

    first commit

---
+++ src/main/java/com/asura/ProgrammingAttenuator.java
@@ -0,0 +1,48 @@
+package com.asura;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+
+import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+
+import java.util.Objects;
+
+
+
+
+public class ProgrammingAttenuator extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        try {
+            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass()
+                    .getResource("gui.fxml")));
+            primaryStage.setTitle("Аттенюатор v1.0");
+            primaryStage.setScene(new Scene(root, 300, 480, Color.web("#3C3F41")));
+            //primaryStage.getIcons().add(new Image("picture/favicon.png"));
+            primaryStage.setResizable(false);
+            primaryStage.show();
+
+            primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+                @Override
+                public void handle(WindowEvent event) {
+                    Platform.exit();
+                    System.exit(0);
+                }
+            });
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
